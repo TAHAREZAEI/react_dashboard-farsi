252787209ff92530ebcd484acc676e34d763be73
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-07 11:06:20 +0330

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
-
+ 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
     <button
@@ -17,13 +17,13 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       onClick={() => customFunc()}
       style={{ color }}
       className="relative text-xl rounded-full p-3 hover:bg-light-gray"
-    >
+    > 
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
       />
       {icon}
-    </button>
+    </button> 
   </TooltipComponent>
 );
 
@@ -32,7 +32,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
-
+ 
     window.addEventListener('resize', handleResize);
 
     handleResize();
@@ -53,7 +53,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
-      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <NavButton title="منو" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
         <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
         <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />} />
@@ -69,9 +69,9 @@ const Navbar = () => {
               alt="user-profile"
             />
             <p>
-              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 text-14">سلام,</span>{' '}
               <span className="text-gray-400 font-bold ml-1 text-14">
-                Michael
+                مایکل
               </span>
             </p>
             <MdKeyboardArrowDown className="text-gray-400 text-14" />
