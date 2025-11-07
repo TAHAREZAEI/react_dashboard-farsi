7fcf80aa8aefb975a2313016c32a2bbbfff8ebad
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-07 16:38:23 +0330

    Update Calendar.jsx

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -21,7 +21,7 @@ const Scheduler = () => {
     arg.navigation.enable = true;
   };
 
-  return (
+  return ( 
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Calendar" />
       <ScheduleComponent
