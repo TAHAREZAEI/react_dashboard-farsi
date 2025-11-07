93b59324c81be9b3bca9efaf3a8083292f23c058
TAHAREZAEI <138865337+TAHAREZAEI@users.noreply.github.com>
2025-11-07 16:28:35 +0330

    Update Area.jsx

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -6,7 +6,7 @@ import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const Area = () => {
-  const { currentMode } = useStateContext();
+  const { currentMode } = useStateContext(); 
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
